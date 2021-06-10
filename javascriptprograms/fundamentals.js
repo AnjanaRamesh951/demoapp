ebcf67e5bd285aa6cdd891929f9321ef8027a7f7
Anjana <2anjanaramesh@gmail.com>
2021-06-10 11:04:57 +0530

    adding js files Date

---
+++ javascriptprograms/fundamentals.js
@@ -0,0 +1,41 @@
+//to display a message in console
+// console.log("hello world")
+ 
+
+
+
+
+//variables to represent a memory locatn
+//jsvariable declartion
+
+//var,let,const
+// var p_name="Anjana Ramesh";
+// var age=23;
+// //our cmpny lumin
+// console.log("hai am",p_name,age,"years old")
+
+
+// var state="kerala"
+// var active_cases=15000;
+// var cured=20000;
+// var death=200;
+// console.log(`in ${state} ${active_cases} new cases and ${cured}`)
+
+
+var company_name="hei"
+var startd=2000;
+var is_active=true
+var rating=4.6
+console.log(typeof(company_name));
+console.log(typeof(startd));
+console.log(typeof(is_active));
+console.log(typeof(rating));
+
+
+
+
+
+
+
+
+
---
+++ javascriptprograms/swapping.js
@@ -0,0 +1,14 @@
+var num1=10,num2=20;
+console.log(`varbles b4 swapping num1=${num1} num2=${num2}`);
+//logic for swapping
+// var temp=num1;
+// num1=num2;//num1=20
+// num2=temp//num2=20
+num1=num1+num2;
+num2=num1-num2;
+num1=num1-num2;
+
+
+console.log(`vals after swaing num1=${num1} um2=${num2} `);
+
+
